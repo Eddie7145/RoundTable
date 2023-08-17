262ced6db0b7aa67135cc1e2138e1141d6fae46f
Eddie7145 <amaachukwuchizzy@gmail.com>
2023-08-17 03:44:59 +0100

    Final Updates

--- frontend/app.js
+++ frontend/app.js
@@ -1,17 +1,27 @@
 const searchBox = document.getElementById("search-box");
 const searchIcon = document.getElementById("searchIcon");
-const bookEvent = document.querySelector("book_event");
-const confirmModal = document.querySelector("event_confirm_dim");
-const closeModal = document.querySelector("close");
 
 searchIcon.onclick = function(){
     searchBox.classList.toggle("active");
+};
+
+function togglePopup(){
+    document.getElementById("popup-1").classList.toggle("active");
 }
 
-function bookevent(){
-    if (bookEvent){
-        confirmModal.classList.remove("hide");
-    } else {
-        confirmModal.classList.add("hide");
-    }
+function sendEmail(){
+let email = document.getElementById("userEmail").value;
+let finalmessage = `Hey ${email} Welcome to the RoundTable <br> Your event has been booked and your ticket ID will be sent to you shortly `;
+
+Email.send({
+    Host : "smtp.elasticemail.com",
+    Username : "amaachukwuchizzy@gmail.com",
+    Password : "CCBCDE17F0D1DD051407AD3C9B86C390B8E9",
+    To : email,
+    From : "amaachukwuchizzy@gmail.com",
+    Subject : "This is the subject",
+    Body : finalmessage,
+}).then(
+  message => alert(message)
+);
 }
